c8fd4bab7fa3696eac5ac0ebd7386bec07a7b52d
Dike Ajah <ekidhaja@gmail.com>
2025-04-28 22:15:57 +0100

    added cryptopay webhooks api

--- src/controllers/WebhooksController/index.ts
+++ src/controllers/WebhooksController/index.ts
@@ -1,13 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import "dotenv/config";
 import { apiResponseHandler } from "@traderapp/shared-resources";
 import { ResponseType } from "../../config/constants";
 import { HttpStatus } from "../../utils/httpStatus";
+import { publishMessageToQueue } from "../../clients/SQSClient/helpers";
 
 export const processCryptopayWebhooks = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		// queue cryptopay webhook
-		console.log("cryptopay webhook received##########", req.body);
-		console.log("cryptopay webhook stringyfied", JSON.stringify(req.body));
+		const queueUrl = process.env.CRYPTOPAY_CHANNELS_WEBHOOKS_QUEUE ?? "";
+		await publishMessageToQueue({ queueUrl, message: req.body });
+		console.log("cryptopay webhook published to queue", { obect: req.body });
 		return res.status(HttpStatus.OK).json(
 			apiResponseHandler({
 				type: ResponseType.SUCCESS,
